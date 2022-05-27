7b91ec3dba21393eca36461dcf71829f8e353afb
abe0929 <93189773+abe0929@users.noreply.github.com>
2022-05-27 16:55:06 +0900

    player

--- Assets/abe/スクリプト/PlayerShot.cs
+++ Assets/abe/スクリプト/PlayerShot.cs
@@ -9,8 +9,9 @@ public class PlayerShot : MonoBehaviour
     public GameObject BulletShot;
     public bool rensyaBousi;
     public float rirod;
-    
-   
+    public static float BulletDI;
+    Rigidbody2D rb2d;
+
     public void Start()
     {
         rensyaBousi = true;
@@ -18,17 +19,27 @@ public class PlayerShot : MonoBehaviour
     }
     public void Update()
     {
+
+
+        //メモ　弾の向きはrb2d.velocity.x > 1　で取って、弾を二個(スプリプも)つくる
+
+        //Vector2 scale = transform.localScale;
+        //if (rb2d.velocity.x > 1)
+        //scale.x = 1;
+        //else if (rb2d.velocity.x < -1)
+        // scale.x = -1;
+        //transform.localScale = scale;
         if (Input.GetButtonDown("Fire1"))
         {
             if (rensyaBousi == true )
             {
                 Vector2 mballpos = BulletShot.transform.position;
                 GameObject newbullet = Instantiate(BulletObj, mballpos, BulletShot.transform.rotation);
-                Vector2 dir = BulletShot.transform.forward;
-
+                //Vector2 dir = BulletShot.transform.forward;
                 //newbullet.GetComponent<Rigidbody2D>().AddForce(dir * BulletSpeed, ForceMode.Impulse);
                 //newbullet.name = BulletObj.name;
                 //Destroy(newbullet, 0.8f);
+                
                 rensyaBousi = false;
                 Invoke("rensya", rirod);
             }
--- Assets/abe/スクリプト/enemyKill.cs
+++ Assets/abe/スクリプト/enemyKill.cs
@@ -6,20 +6,13 @@ public class enemyKill : MonoBehaviour
 {
     public int BulletSpeed = 1;
     public GameObject muzzlePoint;
-    int Speed;
-    //public int enemyHp = 5;//âºÅAìGÇÃåpè≥Ç©ÇÁéùÇ¡ÇƒÇ≠ÇÈ
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        //if(collision.gameObject.tag == "enemy")
-        {
-            //enemyHp -= 1;
-        }
-    }
+    public float bulletDI;
+
     public void Start()
     {
         Invoke("Syatei", 3.0f);
-        //Vector2 mballpos = muzzlePoint.transform.position;
-        //Vector2 dir = muzzlePoint.transform.forward;
+        //bulletDI = PlayerShot.BulletDI;
+
        
     }
     public void Update()
@@ -27,9 +20,6 @@ public class enemyKill : MonoBehaviour
 
         Rigidbody2D rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new(10 * BulletSpeed, 0.0f);
-        
-        
-
     }
     void Syatei() 
     {
