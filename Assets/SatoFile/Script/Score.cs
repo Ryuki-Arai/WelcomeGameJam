0f033a1ce0357ea11cd01c855e784a2deea1e6fa
shun_a123 <sshuna123@gmail.com>
2022-05-27 14:05:52 +0900

    佐藤レイアウトとスクリプト更新

---
+++ Assets/SatoFile/Script/Score.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text Scoretext;
+    int score;
+
+    void Start()
+    {
+
+
+        Scoretext.text = string.Format("Score{0}", score);
+    }
+
+}
