7113f757562e621267bb482e860bc1806f4e2ab9
Ryu-0824 <83778052+Ryuki-Arai@users.noreply.github.com>
2022-05-27 14:20:02 +0900

    GameManager作成

---
+++ Assets/RyukiArai/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    private static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                var obj = new GameObject(typeof(GameManager).Name);
+                _instance = obj.AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    int score;
+
+    public int Score
+    {
+        get => score;
+        set
+        {
+            score = value;
+        }
+    }
+
+    public void SetZero()
+    {
+        score = 0;
+    }
+}
