2d3f10bdc52b1ff2d7940b6d52a3388841433fc5
sinzitoyama <yamat422@gmail.com>
2022-05-27 14:06:45 +0900

    ジャンプと土台完成

--- Assets/Toyama sinzi/Player.cs
+++ Assets/Toyama sinzi/Player.cs
@@ -4,20 +4,42 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    
     public float speed;
     Rigidbody2D rb2d;
+    public float jumpPower = 15f;
 
+
+
+    bool jmp;
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
+        
+
+        if(Input.GetButtonDown("Jump") && jmp)
+        {
+            rb2d.AddForce(Vector2.up * jumpPower,ForceMode2D.Impulse);
+            Debug.Log("aaaaa");
+        }
+       
         float h = Input.GetAxis("Horizontal");
 
         Vector2 a = new Vector2(h,0);
         Vector2 b = a.normalized * speed;
+        b.y = rb2d.velocity.y;
         rb2d.velocity = b;
+    
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        jmp = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        jmp = false;
     }
 }
