c0bade237574c294174fc0920c36709656d429c2
sinzitoyama <yamat422@gmail.com>
2022-05-28 12:27:09 +0900

    タイマー完成

---
+++ Assets/Toyama sinzi/TimeCounter.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class TimeCounter : MonoBehaviour
+{
+    float Timer;
+
+    [SerializeField]  TextMeshProUGUI timer = default;
+    [SerializeField] float totalTime;
+    int seconds;
+    private void Start()
+    {
+        timer = GetComponent<TextMeshProUGUI>();
+        Timer = totalTime;
+    }
+
+    void Update()
+    {
+        Timer -= Time.deltaTime;
+        if (Timer < 0)
+        {
+            SceneManager.LoadScene("Test");
+        }
+        timer.text = Timer.ToString("F0");
+    }
+}
